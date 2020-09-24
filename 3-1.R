db65c47990d55cef8f5b1e55324dfbfd43c2821c
Charlie-kun <56343000+Charlie-kun@users.noreply.github.com>
2020-09-24 20:40:00 +0900

    Create 3-1.R

---
+++ 3-1.R
@@ -0,0 +1,18 @@
+var1 <- c(1,2,4,7,8)   #Create var1 for number of five variate
+var1
+
+var2 <- c(1:5)  #Create var2 for 1 to 5 number.
+var2
+
+var3 <-seq(1,5) #Create var3 for 1 to 5 number.
+var3
+
+var4<- seq(1,10, by=2)  # Numbers from 1 to 10 in 2 increments.
+var4
+
+var5 <- seq(1,10, by=3) #Numbers from 1 to 10 in 3 increments.
+var5
+
+var1+2  #Display for +2 each var1's numbers.
+
+var1+var2   #The sum of each element of var1 and var2.
