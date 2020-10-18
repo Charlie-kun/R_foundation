442ec16edeb08340077b08fbaef516c39fd11053
Charlie-kun <56343000+Charlie-kun@users.noreply.github.com>
2020-10-18 18:57:26 +0900

    Update 6-1.R

--- 6-1.R
+++ 6-1.R
@@ -38,3 +38,21 @@ exam %>% arrange(math)  #increase data
 exam %>% arrange(class, math)  #increase two data
 
 exam %>% arrange(desc(math))  #decrease data
+
+exam %>%
+  mutate(total=math+english+science) %>%
+  head
+
+exam %>%
+  mutate(total=math+english+science,
+         mean=(math+english+science)/3) %>%
+  head
+
+exam %>%
+  mutate(test=ifelse(science >=60, "pass", "fail")) %>%
+  head
+
+exam %>%
+  mutate(total=math+english+science) %>%
+  arrange(total) %>%
+  head
