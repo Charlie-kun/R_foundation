68d2e496ab80ac60181af8426c73f50d874be56f
Charlie-kun <56343000+Charlie-kun@users.noreply.github.com>
2020-11-01 18:17:59 +0900

    Update 6-1.R

--- 6-1.R
+++ 6-1.R
@@ -56,3 +56,21 @@ exam %>%
   mutate(total=math+english+science) %>%
   arrange(total) %>%
   head
+
+exam %>% summarise(mean_math=mean(math))
+
+exam %>%
+  group_by(class) %>%
+  summarise(mean_math=mean(math))
+
+exam %>%
+  group_by(class) %>%
+  summarise(mean_math=mean(math),
+            sum_math=sum(math),
+            median_math=median(math),
+            n=n())
+
+mpg%>%
+  group_by(manufacturer, drv) %>%
+  summarise(mean_cty=mean(cty)) %>%
+  head(10)
