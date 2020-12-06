4d53608c64aa1f7bf5b1f07fab6de2070db06dc3
Charlie-kun <56343000+Charlie-kun@users.noreply.github.com>
2020-12-06 19:35:59 +0900

    Create 8-1.R

---
+++ 8-1.R
@@ -0,0 +1,9 @@
+library(ggplot2)
+
+ggplot(data=mpg, aes(x=displ, y=hwy))
+
+ggplot(data = mpg, aes(x = displ, y=hwy)) + geom_point()
+
+ggplot(data = mpg, aes(x = displ, y=hwy)) + geom_point()+xlim(3,6) #arrange x value 3 to 6
+
+ggplot(data = mpg, aes(x=displ, y=hwy))+geom_point()+xlim(3,6)+ylim(10,30)
