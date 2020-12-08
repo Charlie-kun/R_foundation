090236833963811cb9ff357011637db48a2e8b62
Charlie-kun <56343000+Charlie-kun@users.noreply.github.com>
2020-12-08 21:54:49 +0900

    Update 8-1.R

--- 8-1.R
+++ 8-1.R
@@ -7,3 +7,15 @@ ggplot(data = mpg, aes(x = displ, y=hwy)) + geom_point()
 ggplot(data = mpg, aes(x = displ, y=hwy)) + geom_point()+xlim(3,6) #arrange x value 3 to 6
 
 ggplot(data = mpg, aes(x=displ, y=hwy))+geom_point()+xlim(3,6)+ylim(10,30)
+
+library(dplyr)
+
+df_mpg <- mpg%>%
+  group_by(drv) %>%
+  summarise(mean_hwy=mean(hwy))
+
+df_mpg
+
+ggplot(data=df_mpg, aes(x=drv, y=mean_hwy))+geom_col()
+
+ggplot(data=df_mpg, aes(x=reorder(drv, -mean_hwy), y=mean_hwy))+geom_col()
