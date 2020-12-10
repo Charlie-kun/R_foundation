ca55117753cf9f7de073757a3fe82cab4de27805
Charlie-kun <56343000+Charlie-kun@users.noreply.github.com>
2020-12-10 20:04:13 +0900

    Update 8-1.R

--- 8-1.R
+++ 8-1.R
@@ -19,3 +19,7 @@ df_mpg
 ggplot(data=df_mpg, aes(x=drv, y=mean_hwy))+geom_col()
 
 ggplot(data=df_mpg, aes(x=reorder(drv, -mean_hwy), y=mean_hwy))+geom_col()
+
+ggplot(data=mpg, aes(x=drv))+geom_bar()
+
+ggplot(data=mpg,aes(x=hwy))+geom_bar()
