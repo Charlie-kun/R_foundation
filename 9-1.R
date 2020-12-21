145edcb2d3e45b1b76f965c1326e5c0365028813
Charlie-kun <56343000+Charlie-kun@users.noreply.github.com>
2020-12-21 21:24:45 +0900

    Update 9-1.R

--- 9-1.R
+++ 9-1.R
@@ -116,3 +116,12 @@ ggplot(data=sex_income, aes(x=ageg, y=mean_income, fill=sex))+
 
 ggplot(data=sex_income, aes(x=ageg, y=mean_income, fill=sex))+
   geom_col(position="dodge")+scale_x_discrete(limits=c("young", "middle", "old"))
+
+
+sex_age <- welfare %>%
+  filter(!is.na(income)) %>%
+  group_by(age, sex) %>%
+  summarise(mean_income=mean(income))
+head(sex_age)
+
+ggplot(data=sex_age, aes(x=age, y=mean_income, col=sex))+geom_line()
