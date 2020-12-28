68e49b039eaf382098076267c48b4f0a837fed60
Charlie-kun <56343000+Charlie-kun@users.noreply.github.com>
2020-12-28 20:25:34 +0900

    Update 9-1.R

--- 9-1.R
+++ 9-1.R
@@ -263,3 +263,26 @@ ageg_divorce <- ageg_mariage %>%
 ageg_divorce
 
 ggplot(data=ageg_divorce, aes(x=ageg, y=pct))+geom_col()
+
+ageg_religion_marriage <- welfare %>%
+  filter(!is.na(group_marriage)& ageg !="young") %>%
+  group_by(ageg, religion, group_marriage) %>%
+  summarise(n=n()) %>%
+  mutate(tot_group=sum(n)) %>%
+  mutate(pct=round(n/tot_group*100, 1))
+
+ageg_religion_marriage
+
+ageg_religion_marriage <-welfare %>%
+  filter(!is.na(group_marriage) & ageg !="young") %>%
+  count(ageg, religion, group_marriage) %>%
+  group_by(ageg, religion) %>%
+  mutate(pct=round(n/sum(n)*100, 1))
+
+df_divorce <- ageg_religion_marriage %>%
+  filter(group_marriage == "divorce") %>%
+  select(ageg,religion, pct)
+
+df_divorce
+
+ggplot(data=df_divorce, aes(x=ageg, y=pct, fill=religion)) + geom_col(posision="dodge")
